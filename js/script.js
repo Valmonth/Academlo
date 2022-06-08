20c2795e9fee050194edca1ea6df8f72998942a7
James <51716283+james-commits@users.noreply.github.com>
2022-06-08 10:58:16 -0600

    Cambios

--- js/script.js
+++ js/script.js
@@ -3,3 +3,18 @@ function toggleMenu(){
     nav.classList.toggle('hidden');
 }
 
+var myIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("imgs");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "inline";  
+  setTimeout(carousel, 4000);    
+}
+
